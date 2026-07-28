74306efa7bad53611a8c3013bed957c61348038e
mohamedezzmnu <mohamedezzmnu@gmail.com>
2026-07-28 10:19:42 +0000

    fix: exclude API routes from auth middleware

--- middleware.js
+++ middleware.js
@@ -41,5 +41,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api).*)'],
 };
